a695628a0bb722c52e34024782a1f244accd793c
Robbie1054 <92984438+Robbie1054@users.noreply.github.com>
2025-04-23 11:18:25 +0100

    Added meta images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,18 @@ export const metadata: Metadata = {
     shortcut: "/favicon.svg",
     apple: "/favicon.svg",
   },
+  openGraph: {
+    images: [{
+      url: '/meta/metaimage.png', 
+      width: 1200,
+      height: 630,
+      alt: 'Dashing Distribution Software',
+    }],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    images: ['/meta/metaimage.png'], 
+  },
 };
 
 export default function RootLayout({
